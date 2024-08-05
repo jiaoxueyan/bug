f8108a3f34ac35015893d2a14beab54c3a66f232
jiaoxueyan <2016036930@qq.com>
2024-08-05 17:46:33 +0800

    Create 只有一项时无法选中.vue

---
+++ 只有一项时无法选中.vue
@@ -0,0 +1,29 @@
+<template>
+	value{{value}}
+	<wd-picker-view ref="picker" :columns="storeList" v-model="value" label-key="storeName" value-key="storeId" />
+	<button @click="getList">getList</button>
+	<button @click="getSelects">getSelects</button>
+</template>
+
+<script lang="ts" setup>
+	import {ref} from 'vue'
+	const storeList = ref([])
+	const value = ref<string>('')
+	const getList = () => {
+		storeList.value = [
+			{
+				storeId: 1,
+				storeName: 'aaa'
+			}
+		]
+	}
+	
+	const picker = ref()
+	const getSelects = () => {
+		console.log(picker.value.getValues())
+		console.log(picker.value.getSelects())
+	}
+</script>
+
+<style>
+</style>
